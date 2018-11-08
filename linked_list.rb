107074ede81e5b72703a941247ade176b9f29f01
CheezItMan <chris@adadevelopersacademy.org>
2018-11-08 07:30:57 -0800

    getFirst & addFirst

--- linked_list.rb
+++ linked_list.rb
@@ -5,8 +5,19 @@ class LinkedList
     @head = nil
   end
 
-  def add(value)
+  def addFirst(value)
+    if @head.nil?
+      @head = Node.new(value, nil)
+    else
+      @head = Node.new(value, @head)
+    end
+  end
 
-
+  def getFirst()
+    if @head.nil?
+      return nil
+    else
+      return @head.data
+    end
   end
 end
--- linked_list_spec.rb
+++ linked_list_spec.rb
@@ -8,10 +8,46 @@ describe LinkedList do
 
   describe 'initialize' do
     it 'can be created' do
+      # Arrange & Act
       list = LinkedList.new
 
+      # Assert
       expect(list).must_be_kind_of LinkedList
     end
   end
 
+  describe 'addFirst & getFirst' do
+    # Arrange
+    before do
+      @list = LinkedList.new
+    end
+
+    it 'can add values to an empty list' do
+      # Act
+        @list.addFirst(3)
+
+        # Assert
+        expect(@list.getFirst).must_equal 3
+    end
+
+    it 'will put the last added item to the front of the list' do
+      # Act
+      @list.addFirst(1)
+      @list.addFirst(2)
+
+      # Assert
+      expect(@list.getFirst).must_equal 2
+
+      # Act again
+      @list.addFirst(3)
+
+      # Assert
+      expect(@list.getFirst).must_equal 3
+    end
+
+    it 'will return `nil` for `getFirst` if the list is empty' do
+
+      expect(@list.getFirst).must_be_nil
+    end
+  end
 end
