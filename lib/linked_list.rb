6a42ba82b5c158924bf845d5e38c9818c8f68af1
CheezItMan <chris@adadevelopersacademy.org>
2019-11-14 17:02:56 -0800

    Added time/space complexity questions

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,28 +7,39 @@ class LinkedList
     @head = nil
   end
 
+  # Time complexity - ?
+  # Space complexity - ?
   def add_first(value)
 
   end
 
+  # Time complexity - ?
+  # Space complexity - ?
   def get_first
 
   end
 
+  # Time complexity - ?
+  # Space complexity - ?
   def length
     return 0
   end
 
+  # Time complexity - ?
+  # Space complexity - ?
   def add_last(value)
 
   end
 
+  # Time complexity - ?
+  # Space complexity - ?
   def get_last
 
   end
 
+  # Time complexity - ?
+  # Space complexity - ?
   def get_at_index(index)
 
   end
-
 end
