5566d96922a33e63afe940b68e29f0618e6b5cee
Leah Scott-Zechlin <scottzec@gmail.com>
2020-12-26 19:57:54 -0800

    removed notes

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -88,25 +88,3 @@ class LinkedList
   end
 
 end
-
-
-
-
-# a = Node.new("a")
-# b = Node.new("b")
-# b.next = a
-# c = Node.new("d")
-# c.next = b
-# d = Node.new("d")
-# d.next = c
-# e = Node.new("e")
-# e.next = d
-#
-# head = Node.new("a")
-# # The line below will construct a new node and
-# #   Set it's `next` reference to the old value
-# #   of `head`
-# head = Node.new("b", head)
-# head = Node.new("c", head)
-# head = Node.new("d", head)
-# head = Node.new("e", head)
