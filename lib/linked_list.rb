4aeaa7e973ef8986a111c365dc314e7e1813d6dc
Rachael Gomez <gomezrc1220@gmail.com>
2021-01-06 12:12:18 -0800

    completed all waves

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -41,18 +41,50 @@ class LinkedList
   # Time complexity - ?
   # Space complexity - ?
   def add_last(data)
-
+    current = @head
+    if @head == nil
+      current = Node.new(data)
+      @head = current
+      return
+    end
+    until current == nil
+      previous = current
+      current = current.next
+    end
+    previous.next = Node.new(data)
+    return
   end
 
   # Time complexity - ?
   # Space complexity - ?
   def get_last
+    current = @head
+    if @head == nil
+      return nil
+    end
+    until current == nil
+      previous = current
+      current = current.next
+    end
+    return previous.data
 
   end
 
   # Time complexity - ?
   # Space complexity - ?
   def get_at_index(index)
-
+    count = 0
+    current = @head
+    if @head == nil
+      return nil
+    end
+    until count == index
+      count += 1
+      current = current.next
+    end
+    if current == nil
+      return nil
+    end
+    return current.data
   end
 end
